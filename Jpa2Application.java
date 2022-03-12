b0d774cfc6359a3ea86e30c879d1fd03732f5a36
Vijendra R <mr.vijendrakharwar@gmail.com>
2022-03-12 11:19:30 +0530

    first

---
+++ Jpa2Application.java
@@ -0,0 +1,16 @@
+package com.example.jpa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa2Application {
+
+	public static void main(String[] args) {
+//		Person p = new Person();
+//		p.setFirstName("abc");
+//		int age = p.getAge();
+		SpringApplication.run(Jpa2Application.class, args);
+	}
+
+}
